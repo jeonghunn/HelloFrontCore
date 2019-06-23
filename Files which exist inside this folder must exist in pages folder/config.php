48e24fd4385b0147ea6a8ac88c7b2762355b5d8b
Junghoon Lee <jeonghunn1@gmail.com>
2019-06-23 14:00:59 +0900

    hide unnecessary information.

--- Files which exist inside this folder must exist in pages folder/config.php
+++ Files which exist inside this folder must exist in pages folder/config.php
@@ -4,10 +4,10 @@
 
 $HELLO_FRONT_CORE_VERSION = "1.0";
 $FRONT_APP_VERSION = "3.18.514";
-$DEVELOPMENT_API_URL = "unopenedbox.com/develop/square/";
-$SERVER_API_URL = "s9uare.com/";
-$MAIN_URL = "http://s9uare.com/";
-$MAIN_API_URL = "http://s9uare.com/api.php";
+$DEVELOPMENT_API_URL = "url.com/develop/app/";
+$SERVER_API_URL = "url.com/";
+$MAIN_URL = "http://url.com/";
+$MAIN_API_URL = "http://website.com/api.php";
 $CLIENT_SERVER_IP_ADDRESS = "52.78.110.116";
 $DEVELOPMENT_MODE = true;
 
