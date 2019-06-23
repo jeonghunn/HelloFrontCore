53c45f8396a7c980959e4d1f62a7ad971b128c6e
Junghoon Lee <jeonghunn1@gmail.com>
2019-06-23 13:57:45 +0900

    initial commit.

---
+++ Files which exist inside this folder must exist in pages folder/config.php
@@ -0,0 +1,16 @@
+<?php if(!defined("642979")) exit();
+
+// ini_set('memory_limit', '1024M');
+
+$HELLO_FRONT_CORE_VERSION = "1.0";
+$FRONT_APP_VERSION = "3.18.514";
+$DEVELOPMENT_API_URL = "unopenedbox.com/develop/square/";
+$SERVER_API_URL = "s9uare.com/";
+$MAIN_URL = "http://s9uare.com/";
+$MAIN_API_URL = "http://s9uare.com/api.php";
+$CLIENT_SERVER_IP_ADDRESS = "52.78.110.116";
+$DEVELOPMENT_MODE = true;
+
+
+
+?>
