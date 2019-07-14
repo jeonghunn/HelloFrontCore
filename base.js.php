1bf729444010100dd8f0417cbf61b9b1a3bcfb8c
Junghoon Lee <jeonghunn1@gmail.com>
2019-07-14 15:38:13 +0900

    new core component : base.js.php

---
+++ base.js.php
@@ -0,0 +1,34 @@
+
+
+function getClientVersion(){
+    return "<?php echo getClientVersion(); ?>";
+}
+
+function isDevelopmentMode(){
+    return <?php echo isDevelopmentMode(); ?>;
+}
+
+function getCorePUrl(){
+    return <?php echo getCorePUrl(); ?>;
+}
+
+
+function getAPIUrl(){
+    return <?php echo getAPIUrl(); ?>;
+}
+
+function getAPIUrlS(){
+    return <?php echo getAPIUrlS(); ?>;
+}
+
+
+function getAPISUrl(){
+    return <?php echo getAPISUrl(); ?>;
+}
+
+
+function getClientPUrl(){
+    return <?php echo getClientPUrl(); ?>;
+}
+
+
